d55899ba8d8850789efd0b40c8cb13d1ad4903c7
Your Name <julia.kroger@gmail.com>
2018-10-15 15:19:03 -0300

    adding port

--- server.js
+++ server.js
@@ -96,6 +96,6 @@ app.put('/image', (req, res) =>{
 	.catch(err => res.status(400).json('unable to get entries'))
 })
 
-app.listen(3000, () => {
-	console.log('APP is running')
+app.listen(process.env PORT || 3000, () => {
+	console.log(`APP is running on port ${process.env.PORT`)
 })
